2e85edd1854990d56f27d87d7a474168e1542c0f
Raj Chaurasiya <rc808129@gmail.com>
2025-11-30 15:57:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const removeBtn = document.querySelector('#removeBtn');
 const addBtn = document.querySelector('#addBtn');
 const select = document.getElementById('colorSelect');
 
-let removedOptions = []; // Store removed options
+let removedOptions = [];
 
 removeBtn.addEventListener('click', function(e) {
     e.preventDefault();
@@ -10,30 +10,24 @@ removeBtn.addEventListener('click', function(e) {
     const selectedOption = select.options[select.selectedIndex];
 
     if (selectedOption) {
-        // store copy of removed option
-        removedOptions.push({
-            value: selectedOption.value,
-            text: selectedOption.text
-        });
 
-        // remove from dropdown
+        removedOptions.push(selectedOption)
+
         select.remove(select.selectedIndex);
     }
 });
 
 addBtn.addEventListener('click', function() {
     if (removedOptions.length === 0) {
-        alert("No removed option left!");
+        alert("No removed options left!");
         return;
     }
 
-    // get the last removed option
     const lastOption = removedOptions.pop();
 
-    // create option and add to dropdown
-    const newOption = document.createElement("option");
-    newOption.value = lastOption.value;
-    newOption.text = lastOption.text;
+    // const newOption = document.createElement("option");
+    // newOption.value = lastOption.value;
+    // newOption.text = lastOption.text;
 
-    select.appendChild(newOption);
+    select.appendChild(lastOption);
 });
