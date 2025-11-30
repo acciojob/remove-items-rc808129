30192b3e550e19a2e32099362fd85aa91e415a60
Raj Chaurasiya <rc808129@gmail.com>
2025-11-30 08:52:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const input = document.querySelector('input');
+input.addEventListener('click', function(e) {
+    e.preventDefault();
+    const select = document.getElementById('colorSelect');
+    const selectedOption = select.options[select.selectedIndex];
+
+    if (selectedOption) {
+        select.remove(select.selectedIndex);
+    }
+});
